fffb22ce0d8e0c0e32e0f8cb90a7c8bd38908917
beowulf1889 <anthonyrasband2939@mtec.edu.stu>
2023-11-30 09:02:24 -0700

    added random colors and shapes

--- src/components/ThreeScene.js
+++ src/components/ThreeScene.js
@@ -8,6 +8,8 @@ const ThreeScene = () => {
   const containerRef = useRef();
   let camera, scene, renderer, controls, stats;
 
+  let spheres = [];
+
 
   const clock = new THREE.Clock();
 
@@ -23,7 +25,7 @@ const ThreeScene = () => {
     container.appendChild(renderer.domElement);
 
     // Camera
-    camera = new THREE.PerspectiveCamera(40, width / height, 1, 150000);
+    camera = new THREE.PerspectiveCamera(40, width / height, 1, 1500000);
     camera.position.z = 0;
     camera.position.x = 100;
     camera.position.y = 0;
@@ -31,43 +33,33 @@ const ThreeScene = () => {
     // Scene
     scene = new THREE.Scene();
     // scene.background = new THREE.Color().setHSL(0.51, 0.4, 0.01);
-    // scene.fog = new THREE.Fog(scene.background, 4000, 15000);
+     //scene.fog = new THREE.Fog(scene.background, 4000, 15000);
     // Define the colors for the nebula effect
-const colors = [];
-colors.push(new THREE.Color().setHSL(0.6, 0.5, 0.3));
-colors.push(new THREE.Color().setHSL(0.7, 0.8, 0.6));
-colors.push(new THREE.Color().setHSL(0.8, 0.5, 0.2));
-
-// Define the color stops
-const colorStops = [0, 0.5, 1];
-
-// Create a canvas gradient for the background
-const canvas = document.createElement('canvas');
-canvas.width = 1024;
-canvas.height = 256;
-const ctx = canvas.getContext('2d');
-const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
-
-for (let i = 0; i < colors.length; i++) {
-  gradient.addColorStop(colorStops[i], `rgb(${Math.floor(colors[i].r * 255)}, ${Math.floor(colors[i].g * 255)}, ${Math.floor(colors[i].b * 255)})`);
-}
-
-ctx.fillStyle = gradient;
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-// Use the canvas as a texture for the background
-const texture = new THREE.CanvasTexture(canvas);
-const backgroundMaterial = new THREE.MeshBasicMaterial({ map: texture });
-const backgroundGeometry = new THREE.PlaneGeometry(2, 2, 0);
-
-// Create a plane to render the background
-const backgroundMesh = new THREE.Mesh(backgroundGeometry, backgroundMaterial);
-backgroundMesh.material.depthTest = false;
-backgroundMesh.material.depthWrite = false;
-
-// Set the scene background to the background mesh
-scene.add(backgroundMesh);
-
+    
+    function createRandomGradient() {
+      const colors = [];
+      colors.push(new THREE.Color().setHSL(Math.random(), Math.random(), Math.random()));
+      colors.push(new THREE.Color().setHSL(Math.random(), Math.random(), Math.random()));
+      colors.push(new THREE.Color().setHSL(Math.random(), Math.random(), Math.random()));
+      
+    
+      const colorStops = [0, 0.5, 1];
+    
+      const canvas = document.createElement('canvas');
+      canvas.width = 256;
+      canvas.height = 256;
+      const ctx = canvas.getContext('2d');
+      const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+    
+      colors.forEach((color, i) => {
+        gradient.addColorStop(colorStops[i], `rgb(${Math.floor(color.r * 255)}, ${Math.floor(color.g * 255)}, ${Math.floor(color.b * 255)})`);
+      });
+    
+      ctx.fillStyle = gradient;
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+      return new THREE.CanvasTexture(canvas);
+    }
 
     // Geometry and Material
     const geometry1 = new THREE.BoxGeometry(1000, 1000, 1000, 2, 2, 2,)
@@ -96,26 +88,37 @@ scene.add(backgroundMesh);
     audioLoader.load('sounds/ambient.ogg', function (buffer) {
       sound.setBuffer(buffer);
       sound.setLoop(true);
-      sound.setVolume(0.5);
+      sound.setVolume(0.5);wa
 
       sound.play();
     });
+    function createRandomGeometry() {
+      const geometries = [
+        new THREE.BoxGeometry(100, 100, 100),
+        new THREE.SphereGeometry(250
+          , 12, 12),
+        new THREE.TorusKnotGeometry(50, 20, 16, 100),
+        new THREE.CylinderGeometry(50, 50, 100, 12),
+        new THREE.ConeGeometry(50, 50, 10, 12)
+        // Add more geometries here...
+      ];
 
+      return geometries[Math.floor(Math.random() * geometries.length)];
+    }
 
+    spheres = [];
+    for (let i = 0; i < 600; i++) {
+      const geometry = createRandomGeometry();
+      const material = new THREE.MeshBasicMaterial({ map: createRandomGradient() });
+      const mesh = new THREE.Mesh(geometry, material);
 
-    for (let i = 0; i < 6000; i++) {
-      const mesh = new THREE.Mesh(geometrymesh, new THREE.MeshBasicMaterial({ map: texture }));
-
-
-      // Increase the range for positions to space them farther apart
       mesh.position.set(
-        80000 * (2.0 * Math.random() - 1.0),
-        80000 * (2.0 * Math.random() - 1.0),
-        80000 * (2.0 * Math.random() - 1.0)
+        80000 * (6.0 * Math.random() - 1.0),
+        80000 * (6.0 * Math.random() - 1.0),
+        80000 * (6.0 * Math.random() - 1.0)
       );
 
-      // Increase the scale of each cube to make them larger
-      const scale = Math.random() * 50 + 10; // Adjust the range for larger cubes
+      const scale = Math.random() * 100 + 10;
       mesh.scale.set(scale, scale, scale);
 
       mesh.rotation.set(
@@ -123,34 +126,36 @@ scene.add(backgroundMesh);
         Math.random() * Math.PI,
         Math.random() * Math.PI
       );
-      
+
+       // Assign random song to each sphere
+
+      spheres.push(mesh);
       scene.add(mesh);
-
-
     }
 
     ;
     const geometryStar = new THREE.BoxGeometry(20, 20, 20);
-const materialStar = new THREE.MeshBasicMaterial({ color: 0xffffff, specular: 0xffffff, metalness: 0.9 });
+    const materialStar = new THREE.MeshBasicMaterial({ color: 0xffffff, specular: 0xffffff, metalness: 0.9 });
 
-for (let i = 0; i < 1000; i++) { // Change 10 to the desired number of cubes
-  const star = new THREE.Mesh(geometryStar, materialStar);
-  star.position.set(
-    8000 * (2.0 * Math.random() - 1.0),
-    8000 * (2.0 * Math.random() - 1.0),
-    8000 * (2.0 * Math.random() - 1.0)
-  );
+    // for (let i = 0; i < 10000; i++) { // Change 10 to the desired number of cubes
+    //   const star = new THREE.Mesh(geometryStar, materialStar);
+    //   star.position.set(
+    //     8000 * (12.0 * Math.random() - 1.0),
+    //     8000 * (12.0 * Math.random() - 1.0),
+    //     8000 * (12.0 * Math.random() - 1.0)
+    //   );
 
-  star.rotation.set(
-    Math.random() * Math.PI,
-    Math.random() * Math.PI,
-    Math.random() * Math.PI
-  );
-  
+    //   // star.rotation.set(
+    //   //   Math.random() * Math.PI,
+    //   //   Math.random() * Math.PI,
+    //   //   Math.random() * Math.PI
+    //   // );
 
-  scene.add(star);
-}
 
+    //   scene.add(star);
+    // }
+    
+        
 
 
     // Controls
@@ -190,10 +195,21 @@ for (let i = 0; i < 1000; i++) { // Change 10 to the desired number of cubes
 
 
   function animate() {
-    // mesh.forEach(cube => {
-    //   mesh.rotation.x += 0.01; // Adjust the rotation speed as needed
-    //   mesh.rotation.y += 0.01; // Adjust the rotation speed as needed
-    // });
+    const center = new THREE.Vector3(0, 0, 0);
+
+    spheres.forEach((sphere, index) => {
+      sphere.rotation.y += 0.01;
+      // const angle = Date.now() * 0.001; // Varying angle to make them move at the same speed
+
+      // const x = sphere.position.x + Math.cos(angle) * orbitRadius + sphere.position.x;
+      // const z = sphere.position.z + Math.sin(angle);
+
+      // sphere.position.set(x, sphere.position.y, z);
+    });
+
+
+
+
     renderer.render(scene, camera);
 
 
