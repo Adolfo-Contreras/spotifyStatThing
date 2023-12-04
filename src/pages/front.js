1ec985e196a9c412bb085d2923947e26991817c2
beowulf1889 <anthonyrasband2939@mtec.edu.stu>
2023-12-04 08:33:26 -0700

    chamged to front .js for the main page

---
+++ src/pages/front.js
@@ -0,0 +1,39 @@
+
+import { Inter } from 'next/font/google'
+
+import ThreeScene from '@/components/ThreeScene'
+
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+   
+    <>    
+   
+   <ThreeScene></ThreeScene>
+
+    
+    <div className='h-screen w-screen flex object-contain justify-center static'>
+    
+    <ThreeScene></ThreeScene>
+    
+      
+    
+      
+
+      </div>
+    
+        
+    
+
+    
+
+   
+    </>
+
+    
+   
+  )
+}
