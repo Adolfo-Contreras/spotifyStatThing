affbce9b06c529f8373ac0fe3726782b465554a1
Manuel <126043230+MrsMarcy@users.noreply.github.com>
2023-12-04 08:46:41 -0700

    unlimited pulls

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,8 @@ export default function Home() {
   return (
     <>
     <main>
-    {/* <SearchF></SearchF> */}
-    <RankUI></RankUI>
+    <SearchF></SearchF>
+    {/* <RankUI></RankUI> */}
     </main>
     </>
   )
