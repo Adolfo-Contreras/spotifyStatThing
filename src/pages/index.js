b9c0eb965844ba72d51ec472366657f3f3c6ca3b
Adolfo-Contreras <adolfo.alvaradocontreras1760@stu.mtec.edu>
2023-12-04 10:21:44 -0700

    added three to main

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,14 @@ import { Inter } from 'next/font/google'
 import SearchF from '@/components/search'
 import { RankUI } from '@/components/rankui'
 import { ExampleArtist } from '@/components/SpotifyProfile'
-
+import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
     <main>
+    <Link href="/front">go to three</Link>
     <SearchF></SearchF>
     <RankUI></RankUI>
     </main>
